b8b4f7e0cf7dea70f98873b7d9f72c5c9b39196e
Marcelo Canaparro <marcelocanaparro@gmail.com>
2017-09-25 22:23:09 -0300

    big commit

--- server.js
+++ server.js
@@ -8,11 +8,6 @@ const router = express.Router()
 
 const port = process.env.AVI_PORT || 3001
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
-})
-
 app.listen(port, () => {
     console.log('API server online')
 })
